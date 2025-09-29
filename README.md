5426f28abb35a5240f196fbe2969ad933a2d8338
Sam Switz <sds9bt@gmail.com>
2025-09-29 14:30:59 -0500

    changes

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # micro-research
 getting started with research
+
+## Ideas
+- GPT models have discrete number of classes (tokes), what if tokens could be converted to something continuous?
--- high-dim/hi_dim_exp.r
+++ high-dim/hi_dim_exp.r
@@ -20,3 +20,53 @@ lmod <- lm(y ~ X)
 # Coeffs way different but fitted values are the same
 lmod$fitted.values
 yhat
+
+
+mat <- matrix(c(3,4,4,6), 2, 2)
+egn <- eigen(mat)
+t(matrix(egn$values)) %*% egn$vectors
+egn$vectors %*% t(matrix(egn$values))
+
+
+qrx <- qr(X)
+qrx$qr
+
+
+
+
+
+x <- rnorm(100) #+ rexp(100, rate=4)
+# e <- rexp(100, rate=4)
+y <- rnorm(100)
+
+
+mod <- lm(y ~ x + I(x^2))
+par(mfrow=c(2,2))
+plot(mod)
+
+
+dev.off()
+qqnorm(x)
+qqline(x)
+
+
+
+A <- matrix(c(1, 1, 1, -1), 2, 2)
+
+Ap <- cbind(rbind(A, A), rbind(A, A*-1))
+
+Ap %*% t(Ap)
+
+A %*% t(A)
+
+eigen(A)
+
+
+
+
+A <- matrix(c(1, 2, 3, 4), 2, 2)
+
+t(A) %*% A
+
+
+det(t(A) %*% A)
